c68ad96c018d30a00f0d6ff7106da198ed45da3f
detorhu <rajmanhotra6969@gmail.com>
2026-02-02 20:13:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
-
-AUTO_DELETE = 20
-
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN missing")
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # set in Railway
+CURRENCY = "$"
+MAX_BUILDING_LEVEL = 10
+BASE_INCOME_PER_POP = 2
